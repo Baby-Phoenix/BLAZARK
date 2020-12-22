8b9bb635c301d6770e7ba476675756b5595be13b
Mrinank <mrinank.sivakumar@ontariotechu.net>
2020-12-21 23:44:51 -0500

    Initial Setup
    - Created solution and project files
    - Dependencies added and linked
    - File structure created

---
+++ BLAZARK/Source Files/main.cpp
@@ -0,0 +1,18 @@
+//#include "Game.h"
+
+int main()
+{
+	//Game game("TestOpenGL", 1440, 900, 4, 4, false);
+
+	////MAIN LOOP
+	//while (!game.getWindowShouldClose())
+	//{
+	//	
+	//	//UPDATE INPUT ---
+	//	game.update();
+	//	game.render();
+	//	
+	//}
+
+	return 0; 
+}
